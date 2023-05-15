1ad926e12f27f14552f77a1a9f2adf08178bb95d
baraaAlosaily <seelbaraa@gmail.com>
2023-05-16 00:38:12 +0300

    home page

--- mooncascade-client/src/app/services/page.tsx
+++ mooncascade-client/src/app/services/page.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
 import Layout from '@/components/shared/layout/Layout'
-import React from 'react'
+import TransformModal from '@/components/shared/transformModal/TransformModal';
+import React, { useState } from 'react'
 
 function page() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <Layout>
+    <Layout  handleOpen={handleOpen} >
         <div>Service</div>
+        <TransformModal open={open} handleClose={handleClose} />
     </Layout>
   )
 }
